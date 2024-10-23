c522b6f250d315534fc5b1ca9978c89e5da04d18
eddie-nv <ed.nava.valencia@gmail.com>
2024-10-23 13:36:23 -0700

    Build Optimizations

--- src/layouts/home/MyStack.tsx
+++ src/layouts/home/MyStack.tsx
@@ -1,4 +1,4 @@
-import React, { useRef, useState, useEffect } from 'react'
+import React, { useRef } from 'react'
 import Flow from '@/components/Flow'
 import { Box, Stack, Title } from '@mantine/core'
 import { getHeroFlowData } from '@/utils/flowData'
@@ -6,26 +6,8 @@ import { getHeroFlowData } from '@/utils/flowData'
 
 const MyStack = () => {
     const flowWrapperRef = useRef<HTMLDivElement>(null);
-    // const [dimensions, setDimensions] = useState({ width: 0, height: 0 });
 
-    const { heroNodes, heroEdges } = getHeroFlowData();
-
-    
-    // useEffect(() => {
-    //     const updateDimensions = () => {
-    //       if (flowWrapperRef.current) {
-    //         setDimensions({
-    //           width: flowWrapperRef.current.offsetWidth,
-    //           height: flowWrapperRef.current.offsetHeight,
-    //         });
-    //       }
-    //     };
-    
-    //     updateDimensions();
-    //     window.addEventListener('resize', updateDimensions);
-    
-    //     return () => window.removeEventListener('resize', updateDimensions);
-    //   }, []);    
+    const { heroNodes, heroEdges } = getHeroFlowData(); 
         
     return (
         <Stack mt={50} gap={50}>
