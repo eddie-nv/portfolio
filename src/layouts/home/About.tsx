b8b2ec12f3abd4e76bb0795797049137209104ce
eddie-nv <ed.nava.valencia@gmail.com>
2024-11-22 14:00:45 -0800

    Home Page Update (#26)

--- src/layouts/home/About.tsx
+++ src/layouts/home/About.tsx
@@ -1,11 +1,13 @@
-import { Stack, Title, Group, AspectRatio, Box, Text } from '@mantine/core'
+import { Stack, Title, Group, AspectRatio, Text } from '@mantine/core'
 import { useMediaQuery } from '@mantine/hooks'
+import Image from 'next/image'
 import React from 'react'
+import eduardo from '/public/images/pro_pic.png'
 
 const About = () => {
     const isTablet = useMediaQuery('(max-width: 53em)')
   return (
-    <Stack pb={150} mt={200} gap={100} >
+    <Stack pb={90} mt={200} gap={100} >
         <Title order={2} >
             ABOUT
         </Title>
@@ -18,10 +20,8 @@ const About = () => {
                     I&apos;m a new resident of North Beach SF, and I&apos;m here to be surrounded by the best in the world. I set out to learn programming 3 years ago and in that time I gained a deep understanding of frontend techniques. 
                 </Text>
             </Stack>
-            <AspectRatio ratio={0.7} w={200} bg='gray'>
-                <Box>
-
-                </Box>
+            <AspectRatio ratio={0.76} w={330} bg='gray' style={{borderRadius: 5, overflow: 'hidden'}}>
+                <Image src={eduardo} alt='Eduardo' style={{objectFit: 'cover', width: '100%', height: '100%'}}/>
             </AspectRatio>
         </Group>
     </Stack>
